7ae884dead48bc38357643d2abf5e0f5d15b82da
Felipe Okino <felipe1okino@live.com>
2023-09-02 02:20:15 -0300

    feat: created loader component

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,9 @@
+import { LoaderContainer, PulseLoader } from './styles';
+
+export default function Loader() {
+  return (
+    <LoaderContainer>
+      <PulseLoader />
+    </LoaderContainer>
+  );
+}
