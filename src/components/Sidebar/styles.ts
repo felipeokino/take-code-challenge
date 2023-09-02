666c69d6912e2b7163f9c9478da66bb5382397e5
Felipe Okino <felipe1okino@live.com>
2023-09-02 02:05:26 -0300

    feat: created sidebar component

---
+++ src/components/Sidebar/styles.ts
@@ -0,0 +1,80 @@
+import { HtmlHTMLAttributes } from 'react';
+import styled, { css } from 'styled-components';
+
+// --------------- ROOT -------------------
+
+type ContainerProps = HtmlHTMLAttributes<HTMLDivElement> & {
+    isCollapsed: boolean
+}
+export const Container = styled.nav<ContainerProps>`
+    width: 280px;
+    height: 100vh;
+    padding: 1.5rem;
+
+    background-color: #FFF;
+    color: #000;
+
+    position: fixed;
+    left: 0;
+    top: 72px;
+    display: flex;
+    flex-direction: column;
+    box-sizing: border-box;
+    gap: 8px;
+    
+    transition: width 0.3s ease-in-out;
+    opacity: 1;
+
+    z-index: 1;
+    * {
+        transition: all 0.3s ease-in-out;
+    }
+    ${props => !props.isCollapsed && css`
+        width: 89px;
+
+        div {
+            width: 40px;
+        }
+        span {
+            opacity: 0;
+            user-select: none;
+            pointer-events: none;
+            cursor: default;
+        }
+    `}
+`;
+
+
+// ----------------- ITEM -------------------
+type SideBarItemProps = {
+    isActive?: boolean;
+}
+export const SideBarItem = styled.div<SideBarItemProps>`
+    width: 100%;
+    height: 40px;
+    padding: 8px;
+    box-sizing: border-box;
+
+    border-radius: 6px;
+
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 8px;
+
+    font-weight: 600;
+    
+    cursor: pointer;
+
+    img {
+        filter: invert(1);
+    }
+
+    ${props => props.isActive && css`
+        background-color: #0065FF;
+        color: #FFFFFF;
+        img {
+            filter: invert(0);
+        }
+    `}
+`;
