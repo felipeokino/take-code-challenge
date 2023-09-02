24cd54cf9cf81c9e3ef4036d5b7dbdd932ded706
Felipe Okino <felipe1okino@live.com>
2023-09-02 02:10:43 -0300

    feat: add header component

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.header`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 72px;
+    color: #000;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 12px;
+    box-sizing: border-box;
+    z-index: 2;
+    background-color: #FFFFFF;
+    div {
+        display: flex;
+        gap: 8px;
+
+        .logo, .settings, .notifications {
+            width: 30px;
+            height: 30px;
+            border-radius: 50%;
+            background-color: aquamarine;
+        }
+    }
+`;
