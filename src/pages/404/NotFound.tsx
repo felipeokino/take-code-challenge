73aeac99b3243186425d7bd8bd5ec1a727670962
Felipe Okino <felipe1okino@live.com>
2023-09-03 03:42:20 -0300

    fix: fixed not found page and title text

--- src/pages/404/NotFound.tsx
+++ src/pages/404/NotFound.tsx
@@ -7,7 +7,7 @@ export default function NotFound() {
     <NotFoundContainer>
       <img src={NotFoundImage} alt='Uma imagem cartoonizada de um carro sendo rebocado por um caminhão guincho'/>
       <h1>Ops....</h1>
-      <p>Me desculpe, mas não consegui encontrar.</p>
+      <p>Me desculpe, mas não consegui encontrar esta página.</p>
     </NotFoundContainer>
   );
 }
