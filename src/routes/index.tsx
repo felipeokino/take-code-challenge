d6fe5e28b5342541ccd5a24ce46968ad188739ca
Felipe Okino <felipe1okino@live.com>
2023-09-03 21:24:33 -0300

    chore: update packages list

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,16 +2,11 @@ import { Route,Routes } from 'react-router-dom';
 
 import NotFound from '../pages/404/NotFound';
 import Vehicles from '../pages/Vehicles';
-import VehiclesDetails from '../pages/VehiclesDetails';
 
 const routes = [
   {
     path: '/vehicles',
     component: Vehicles
-  },
-  {
-    path: '/vehicles/:id',
-    component: VehiclesDetails
   },
   {
     path: '*',
